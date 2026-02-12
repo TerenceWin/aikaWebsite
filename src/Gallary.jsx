8da3a9d5c6ad2a14e0c1cbe08d897c913809131e
TerenceWin <105475764+TerenceWin@users.noreply.github.com>
2026-02-12 19:48:20 +0900

    Completed the Gallary Page

--- src/Gallary.jsx
+++ src/Gallary.jsx
@@ -1,16 +1,43 @@
 import { useState } from 'react'
 import './Gallary.css'
+import gallary1_1 from '../public/images/gallary1_1.jpeg'
+import gallary1_2 from '../public/images/gallary1_2.jpeg'
+import gallary1_3 from '../public/images/gallary1_3.jpeg'
+import gallary1_4 from '../public/images/gallary1_4.jpeg'
+import gallary1_5 from '../public/images/gallary1_5.jpeg'
 
 function Gallary(){
     const [gallaryItem, setGallaryItem] = useState(null);
-
-    const gallaryItemClicked = (item) => {
-      setGallaryItem(item)
-    }
+    const [currentDisplayIndex, setCurrentDisplayIndex] = useState(0);
 
     const gallary = ["gallary1"] //"gallary2", "gallary3", "gallary4", "gallary5"
     const gallaryDiscription = ["gallary1 text description"] //, "gallary2 text description", "gallary3 text description", "gallary4 text description", "gallary5 textr description"
-    
+    const gallaryArr1 = [gallary1_1, gallary1_2, gallary1_3, gallary1_4, gallary1_5]
+
+    const previous = "<<"
+    const next = ">>"
+
+    const gallaryItemClicked = (item) => {
+      setGallaryItem(item)
+      setCurrentDisplayIndex(0)
+    }
+
+    const previousGallaryItem = () => {
+      if(currentDisplayIndex == 0){
+        setCurrentDisplayIndex(gallaryArr1.length - 1)
+      }else{
+        setCurrentDisplayIndex(currentDisplayIndex - 1)
+      }
+    }
+
+    const nextGallaryItem = () => {
+      if(currentDisplayIndex == (gallaryArr1.length - 1)){
+        setCurrentDisplayIndex(0)
+      }else{
+        setCurrentDisplayIndex(currentDisplayIndex + 1)
+      }
+    }
+
     return(
         <div id="gallary" className="gallary">
             {gallary.map((item, index) => {
@@ -28,9 +55,17 @@ function Gallary(){
             {gallaryItem && (
               <div className="popup-wrapper">
                 <div id="gallary-item-container" className="gallary-item-container">
-                  <h1>GallaryItem </h1>
+                    <button className="close-btn" onClick={() => setGallaryItem(null)}>X</button>
+                    
+                    <div className="gallary-context">
+                      <button className="previous-button" onClick={() => previousGallaryItem()}>{previous}</button>
+                      <div id="gallary-library" className="gallary-library">
+                        <img className="current-gallary-img" src={gallaryArr1[currentDisplayIndex]} />
+                      </div>
+                      <button className="next-button" onClick={() => nextGallaryItem()}>{next}</button>
+                    </div>
+                    
                 </div>
-                <button className="close-btn" onClick={() => setGallaryItem(null)}>Close</button>
               </div>
             )}
         </div>
